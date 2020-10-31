154906ff9f3cb609a314adf2883118180ec57446
artik777 <arturpojarkov91@gmail.com>
2020-10-31 16:12:36 +0200

    nuxt admin

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "About"
+    };
+  },
+  methods: {
+    onClick() {}
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ pages/owner.vue
@@ -0,0 +1,108 @@
+<template>
+  <main>
+    <div class="container-fluid c-section">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <div class="a-spacing-top-medium"></div>
+          <h2>Add a new owner</h2>
+          <form>
+            <!-- Name -->
+            <div class="a-spacing-top-medium">
+              <label>Name</label>
+              <input
+                type="text"
+                class="a-input-text"
+                style="width: 100%"
+                v-model="name"
+              />
+            </div>
+            <!-- About -->
+            <div class="a-spacing-top-medium">
+              <label>About</label>
+              <input
+                type="text"
+                class="a-input-text"
+                style="width: 100%"
+                v-model="about"
+              />
+              <!-- photo file -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-button: 0px;">Add photo</label>
+                <div class="a-row a-spacing-top-medium">
+                  <label class="choosefile-button">
+                    <i class="fal fa-plus"></i>
+                    <input type="file" @change="onFileSelected" />
+                    <p>{{ fileName }}</p>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="a-spacing-top-large">
+              <span class="a-button-register">
+                <span class="a-button-inner">
+                  <span class="a-button-text" @click="onAddOwner"
+                    >Add Owner</span
+                  >
+                </span>
+              </span>
+            </div>
+          </form>
+          <br />
+          <ul class="list-group-item">
+            <li v-for="owner in owners" :key="owner._id">
+              {{ owner.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    try {
+      const response = await $axios.$get("http://localhost:3000/api/owners");
+      return {
+        owners: response.owners
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  data() {
+    return {
+      name: "",
+      about: "",
+      selectedFile: null,
+      fileName: ""
+    };
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      this.fileName = event.target.files[0].name;
+    },
+    async onAddOwner() {
+      try {
+        let data = new FormData();
+        data.append("name", this.name);
+        data.append("about", this.about);
+        data.append("photo", this.selectedFile, this.selectedFile.name);
+        const response = await this.$axios.$post(
+          "http://localhost:3000/api/owners",
+          data
+        );
+        this.owners.push(this.name);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
